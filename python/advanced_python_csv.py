3f7aeb2c86e7f18c657e1f4bbd5ccc1308b831a3
TorrBorr <victoria.aston@gmail.com>
2017-03-26 21:45:57 -0700

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,14 @@
-PLACE YOUR CODE HERE
+import csv
+
+def write_emails(file):
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        emails = []
+        for row in reader:
+            emial_address = row[" email"]
+            emails.append(emial_address)
+        
+    writer = csv.writer((open("emails.csv","w")),delimiter="\n")
+    writer.writerow(emails)
+
+write_emails('faculty.csv')
